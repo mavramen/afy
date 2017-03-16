d7fefd7d399b3e46d4d2f856a00cd76fc2ef5262
mavramen <maya.avramenko@gmail.com>
2017-03-16 16:43:25 -0700

    3-16-17

--- web/assets/js/afy.js
+++ web/assets/js/afy.js
@@ -1,7 +1,15 @@
+
 $(document).ready(function () {
-    $("#footer-container").load("footer.html");
+
+    /* <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->  */
+    //$("head").append('<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries --><!-- WARNING: Respond.js does not work if you view the page via file:// --><!--[if lt IE 9]><script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>//<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->');
+
+    $("#footer-container").load("footer.html", function () {
+        $("#year").text(new Date().getFullYear());
+    });
+
     $("#nav-container").load("navigation.html", function () {
-        var menu = ['home', 'about', 'community', 'resources', 'shopping', 'contact'];
+        var menu = ['home', 'about', 'photo', 'community', /*'resources',*/ 'shopping', 'contact'];
         $("#top-navbar-ul>li").removeClass("active");
         $.each(menu, function (index, value) {
             //alert(index + ": " + value);
@@ -10,4 +18,12 @@ $(document).ready(function () {
             }
         });
     });
+
+
+});
+
+jQuery.fn.extend({
+    loadDonate: function () {
+        $("#donation-container").load("callToDonation.html");
+    }
 });
