6703a1e55f80593876793ba9a2b7c12a4277e848
mavramen <maya.avramenko@gmail.com>
2017-08-03 11:10:59 -0700

    Ауг 3, 2017

--- web/assets/js/afy.js
+++ web/assets/js/afy.js
@@ -1,5 +1,10 @@
+//var oldURL="https://afypa.org/";  //72.52.73.152
+var oldURL="http://72.52.73.152/";  
+
 
 $(document).ready(function () {
+    
+    
 
     /* <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->  */
     //$("head").append('<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries --><!-- WARNING: Respond.js does not work if you view the page via file:// --><!--[if lt IE 9]><script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>//<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->');
@@ -25,5 +30,30 @@ $(document).ready(function () {
 jQuery.fn.extend({
     loadDonate: function () {
         $("#donation-container").load("callToDonation.html");
+    },
+    equalHeightFor: function (selectorChild) {
+        var diff = 0;
+        $(this).each(function (index) {
+            if (index > 0) {
+                $(this).find(selectorChild).css('height', diff - $(this).innerHeight() + $(this).find(selectorChild).innerHeight() + 'px');
+            } else {
+                diff = $(this).innerHeight();
+            }
+
+        });
+    },
+    equalHeightMax: function () {
+        var max = 0;
+        $(this).each(function () {           
+            var h=$(this).innerHeight();          
+            if (max<h){
+                max=h;
+            }        
+        });
+        
+        $(this).each(function (index) {          
+            $(this).css('height', max + 'px');
+        });
+        
     }
 });
